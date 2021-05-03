d95e2c4be8f2fc9d74be90247899d8c961adc9c1
Sagar Shrestha <sagarshth@gmail.com>
2021-05-03 14:52:30 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import React, { useState } from 'react';
+import QRCode from './QRCode'
+
 import "./styles.css";
 
 export default function App() {
+
+  const [url, setUrl] = useState();
+  
+ 
+  function generateQRCode(){
+    let website = document.getElementById("website").value;
+  
+    if (website) {
+    
+      setUrl(website)
+
+      document.getElementById("qrcode-container").style.display = "block";
+    
+
+    }
+    else{
+      alert("Please enter a valid URL");
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="form">
+      <h1>QR Code using qr.js</h1>
+        <input type="url" id="website" name="website" 
+               placeholder="https://webisora.com" required />
+        <button type="button" onClick={generateQRCode}>
+          Generate QR Code
+        </button>
+
+      <div id="qrcode-container">
+        <hr/>
+        {url ? 
+        <>
+          <QRCode value = {url} foreground="#5868bf" />
+        </>
+        : null 
+        }
+        
+      </div>
+    </div>
     </div>
   );
 }
